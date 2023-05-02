97567f003fe0e345796c231ea830930540bf1526
Bappy <bappymahmud321@gmail.com>
2023-05-02 12:39:54 +0600

    added component

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Pages/Home/Home/Shared/Header/Header';
+import Footer from '../Pages/Home/Home/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
