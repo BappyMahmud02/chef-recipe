ed55beeea54d986b55e408c72d916991b0f0962c
Bappy <bappymahmud321@gmail.com>
2023-05-04 20:56:59 +0600

    added footer

--- src/Pages/Home/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Home/Shared/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import Login from '../../../../Login';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Register from '../../../../Register';
 import { AuthContext } from '../../../../../Providers/Authproviders';
 
@@ -27,19 +27,19 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold">
-                            <li><Link to='/'>Home</Link></li>
+                            <NavLink to='/'>Home</NavLink>
 
-                            <li><Link to='/blog'>Blog</Link> </li>
+                            <NavLink to='/blog'>Blog</NavLink> 
                         </ul>
                     </div>
                     <Link to='/' className="btn btn-ghost font-bold normal-case text-2xl md:text-4xl text-emerald-400 "> <span className='text-green-700	'>Best</span> Eats</Link>
 
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal text-2xl font-bold px-1 ">
-                        <li><Link to='/'>Home</Link></li>
+                    <ul className="menu menu-horizontal gap-4 text-2xl font-bold px-1 ">
+                       <NavLink to='/'>Home</NavLink>
 
-                        <li><Link to='/blog'>Blog</Link></li>
+                        <NavLink to='/blog'>Blog</NavLink>
                     </ul>
                 </div>
                 <div className="navbar-end">
